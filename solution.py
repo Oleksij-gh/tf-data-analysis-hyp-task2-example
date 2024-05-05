92710c064768ee042c8592bd01301341fb784e0b
Aleksei <72971918+Oleksij-gh@users.noreply.github.com>
2024-05-05 22:32:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 581150379 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+    alpha = 0.05
+    
+    stat, p_value = mannwhitneyu(x, y, alternative='two-sided')
+    return p_value > alpha
